8a819fbca441fb5a92233299a3dce777151db9e8
xiaotang158 <66454911+xiaotang158@users.noreply.github.com>
2024-07-11 05:05:56 +0800

    Update EasyHooker.java

--- app/src/main/java/com/zqstudio/easyxposed/EasyHooker.java
+++ app/src/main/java/com/zqstudio/easyxposed/EasyHooker.java
@@ -24,7 +24,7 @@ public final class EasyHooker implements IXposedHookLoadPackage {
 	}
 
 	private void appHook(){
-		Class test = clazzForName("test.a.b.c");
+		Class test = clazzForName("io.virtualapp.sandvxposed64");
 		hookMethod("com.ironsource.sdk.controller.IronSourceWebView$JSInterface",
 				"onAdWindowsClosed", test, new XC_MethodHook() {
 					@Override
@@ -33,5 +33,26 @@ public final class EasyHooker implements IXposedHookLoadPackage {
 						showStack();
 					}
 				});
+		hookMethod(LocationManager.class, "isProviderEnabled", String.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                String provider = (String) param.args[0];
+                if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                    XposedBridge.log("Spoofing GPS enabled status for provider: " + provider);
+                    param.setResult(true); 
+                }
+            }
+        });
+	hookMethod(Settings.Secure.class, "getInt", ContentResolver.class, String.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                String name = (String) param.args[1];
+                if (name.equals("location_mode")) {
+                    XposedBridge.log("Settings.Secure.getInt called with name: " + name + " and default value: " + param.args[2]);
+                    XposedBridge.log("Spoofing location_mode as enabled");
+                    param.setResult(3); 
+                }
+            }
+        });
 	}
 }
